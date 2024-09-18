61c91d7c64669e956bb153f9cdae2d524252e243
Kwik Jonathan <kwikandreasjonathan@gmail.com>
2024-09-19 03:48:32 +0700

    update

--- app/components/Struktur.tsx
+++ app/components/Struktur.tsx
@@ -1,39 +1,48 @@
 const Struktur: React.FC = () => {
   const dosenPembina = [
-    { img: "dosen1.jpg", name: "Agustinus Fritz Wijaya, S.Kom, M.Cs" },
-    { img: "dosen2.jpg", name: "Dr. Fransiskus Adikara, S.Kom, M.M" },
-    { img: "dosen3.jpg", name: "Jusia Amanda Ginting S.Kom, M.M" },
-    { img: "dosen4.jpg", name: "Teady Matius Surya Mulyana, S.Kom, M.Kom" },
+    {
+      img: "/public/static/bios.png",
+      name: "Agustinus Fritz Wijaya, S.Kom, M.Cs",
+    },
+    {
+      img: "/public/static/bios.png",
+      name: "Dr. Fransiskus Adikara, S.Kom, M.M",
+    },
+    { img: "/public/static/bios.png", name: "Jusia Amanda Ginting S.Kom, M.M" },
+    {
+      img: "/public/static/bios.png",
+      name: "Teady Matius Surya Mulyana, S.Kom, M.Kom",
+    },
   ];
 
   const ketuaDivisi = [
-    { img: "ketua_divisi1.jpg", name: "NICI ANDREAS" },
-    { img: "ketua_divisi2.jpg", name: "Nama Ketua Divisi PnD" },
-    { img: "ketua_divisi3.jpg", name: "Christopher Haris" },
+    { img: "/public/static/bios.png", name: "NICI ANDREAS" },
+    { img: "/public/static/bios.png", name: "Nama Ketua Divisi PnD" },
+    { img: "/public/static/bios.png", name: "Christopher Haris" },
   ];
 
   const anggotaHumas = [
-    { img: "anggota_divisi1_1.jpg", name: "Nama Anggota Divisi Humas 1" },
-    { img: "anggota_divisi1_2.jpg", name: "Nama Anggota Divisi Humas 2" },
-    { img: "anggota_divisi1_3.jpg", name: "Nama Anggota Divisi Humas 3" },
-    { img: "anggota_divisi1_4.jpg", name: "Nama Anggota Divisi Humas 4" },
-    { img: "anggota_divisi1_5.jpg", name: "Nama Anggota Divisi Humas 5" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi Humas 1" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi Humas 2" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi Humas 3" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi Humas 4" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi Humas 5" },
   ];
 
   const anggotaPND = [
-    { img: "anggota_divisi1_1.jpg", name: "Nama Anggota Divisi PND 1" },
-    { img: "anggota_divisi1_2.jpg", name: "Nama Anggota Divisi PND 2" },
-    { img: "anggota_divisi1_3.jpg", name: "Nama Anggota Divisi PND 3" },
-    { img: "anggota_divisi1_4.jpg", name: "Nama Anggota Divisi PND 4" },
-    { img: "anggota_divisi1_5.jpg", name: "Nama Anggota Divisi PND 5" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi PND 1" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi PND 2" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi PND 3" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi PND 4" },
+    { img: "/public/static/bios.png", name: "Nama Anggota Divisi PND 5" },
   ];
 
   const anggotaDNP = [
-    { img: "anggota_dnp1.jpg", name: "Kwik Andreas Jonathan" },
-    { img: "anggota_dnp2.jpg", name: "Steven Sebastian" },
-    { img: "anggota_dnp3.jpg", name: "Raffael Bernard Wijaya" },
-    { img: "anggota_dnp4.jpg", name: "Alvyn Vinardy Salim" },
-    { img: "anggota_dnp5.jpg", name: "Joshua Immanuel Nicholas" },
+    { img: "/public/static/bios.png", name: "Kwik Andreas Jonathan" },
+    { img: "/public/static/bios.png", name: "Steven Sebastian" },
+    { img: "/public/static/bios.png", name: "Raffael Bernard Wijaya" },
+    { img: "/public/static/bios.png", name: "Alvyn Vinardy Salim" },
+    { img: "/public/static/bios.png", name: "Joshua Immanuel Nicholas" },
   ];
 
   return (
